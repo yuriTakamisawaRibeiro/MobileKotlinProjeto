44357fb6c966c9a286f432a8a0d3bfb76af2927a
Yuri Takamisawa Ribeiro <110643200+yuriTakamisawaRibeiro@users.noreply.github.com>
2024-06-16 15:30:00 -0300

    Read userProfile

--- app/src/main/java/com/example/projetomobile/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/projetomobile/activities/UserProfileActivity.kt
@@ -1,11 +1,15 @@
 package com.example.projetomobile.activities
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projetomobile.screens.Profile
 
 class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            Profile()
+        }
     }
 }
--- app/src/main/java/com/example/projetomobile/inflaters/UserProfileXmlLayout.kt
+++ app/src/main/java/com/example/projetomobile/inflaters/UserProfileXmlLayout.kt
@@ -1,21 +1,42 @@
 package com.example.projetomobile.inflaters
 
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.projetomobile.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 @Composable
 fun UserProfileXmlLayout() {
     val context = LocalContext.current
-    
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser
+
+
     AndroidView(factory = { context ->
         val view = LayoutInflater.from(context).inflate(R.layout.activity_user_profile, null, false)
+
+        getUserProfileEmail(view, currentUser)
+
         view
-    },
-        modifier = Modifier.fillMaxSize())
+    }, modifier = Modifier.fillMaxSize())
+
+
 }
+
+// colocar funções de crud no inflater para ele renderiza-las tambem
+private fun getUserProfileEmail(view : View, currentUser: FirebaseUser?) {
+    currentUser?.let {
+        val emailEditText = view.findViewById<EditText>(R.id.textUserEmail)
+        emailEditText.setText(currentUser.email)
+        emailEditText.isEnabled = false
+    }
+}
+
